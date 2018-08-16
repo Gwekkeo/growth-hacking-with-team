b0d17f3879c94515800ee3b61829ec5f1c8c80df
Gwekkeo <gwendoline.cassand@gmail.com>
2018-08-16 17:12:57 +0200

    prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
-  config.action_mailer.default_url_options = { :host => 'kwego.rs@gmail.com' }
+  config.action_mailer.default_url_options = { :host => 'mailchimp.com' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp

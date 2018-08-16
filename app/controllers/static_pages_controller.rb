4a18c870edf5db551564e0621d5ea3c955470837
Gwekkeo <gwendoline.cassand@gmail.com>
2018-08-16 13:47:57 +0200

    ajout ligne controller_create envoi un email

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,6 +5,10 @@ class StaticPagesController < ApplicationController
   def create
   	mon_user = User.new(first_name: params[:first_name], last_name: params[:last_name], email: params[:email])
   	if mon_user.save
+  		# LIGNE POUR ENVOYER UN EMAIL AVEC LE NOM
+  		# DE LA PERSONNE
+  		ContactMailer.contact(mon_user.first_name).deliver_later
+
   		redirect_to root_path
   	end
   end

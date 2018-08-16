c3588774db271a8305036fedd0d4f24910e7ff6c
Joanne <jonane_k3ly@hotmail.fr>
2018-08-16 20:53:52 +0200

    ajout bot twitter

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ gem 'bootstrap', '~> 4.1.3'
 gem 'jquery-rails'
 gem 'dotenv'
 gem 'gibbon'
+gem 'twitter'
+gem 'pry'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.1'
---
+++ app/services/bot_twitter.rb
@@ -0,0 +1,65 @@
+require "twitter"	#
+require "dotenv"	#
+Dotenv.load			#
+#####################
+
+class BotTwitter
+#------------------------ CONNEXION --------------------------
+
+attr_accessor :client
+
+	def initialize
+	# client = Twitter::Streaming::Client.new do |config|
+		@client = Twitter::REST::Client.new do |config|
+			# config.consumer_key		  = ENV["consumer_key"]
+			# config.consumer_secret	  = ENV["consumer_secret"]
+			# config.access_token 	  = ENV["access_token"]
+			# config.access_token_secret = ENV["access_token_secret"]
+
+			config.consumer_key		  = "0XnaIM711lbpSFwOgAXjceX9t"
+			config.consumer_secret	  = "Y6KS4qnlhFPwa4EgXIRBOp45NoF6XlRMoHST9HP7LxwdTd0V4H"
+			config.access_token 	  = "1016978227586306048-UzXGQ0yLswqNjSfcQK94ejtNC7gHqr"
+			config.access_token_secret = "d4AGaUGxYKILfaJ8bX4Mzs8LI12YCOtmF2IVrDSBMz7Cl"
+		end
+	# puts @client
+	end
+
+
+#---------- Le bot envoie un tweet à des users suivant une recherche spécifique ---------------
+
+	def perform
+
+	#--- ECHANTILLON 1 : Envoi des tweets ---	
+		# team = ["@Gwen_Rub1", "@Rubyaskip"]
+		# team.each do |username|
+		# 	client.update("#{username} nouveau test de message 13:41")
+		# 	# sleep 87
+		# end
+
+	#--- ECHANTILLON 2 : Récup des tweets ---
+		# @client.search("#bonanniversaire" -rt).take(3).each do |tweet|
+		# 	puts tweet.text
+		# end
+
+	#--- ECHANTILLON 3 ---		
+		@tweetsid = []
+		@client.search("#ruby -rt", result_type: "recent").take(10).each do |tweet|
+			@tweetsid << tweet
+			#puts @tweetsinfos[0].user.screen_name
+		end
+		
+		@usernames = []
+		@tweetsid.each do |userid|	
+			@usernames << userid.user.screen_name
+		end
+			#puts @usernames
+		
+		@usernames.each do |username|
+			@client.update("Hello @#{username} ! Je t'invite à découvrir le site de THP https://www.thehackingproject.org/ ! Enjoy !")
+			# sleep 2
+		end
+		puts "Tweets postés"
+		
+	end #end perform 
+
+end #end class
